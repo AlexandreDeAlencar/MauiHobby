d188000553dae2e942ad03777652f1a456dc8443
Alexandre de Alencar Ferreira <a.alencar@ga.agr.br>
2023-02-20 13:58:11 -0300

    MAUI client layer created

---
+++ GA+Intergado.CR2.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_Intergado.CR2.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GA+Intergado.CR2.MAUI/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using GA_Intergado.CR2.App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace GA_Intergado.CR2.MAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(MainPage).Assembly));
+            builder.Services
+                .AddApplication()
+                .AddIntegrationApi2();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
