e7a73a0fb4cef115a7c6e39f141c9559f5477ac9
Alexandre de Alencar Ferreira <a.alencar@ga.agr.br>
2023-03-30 01:22:08 -0300

    feature: external device layer added

---
+++ GA+Intergado.CR2.ExternalDevices/DependencyInjectionRegister.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GA_Intergado.CR2.App
+{
+    public static class DependencyInjectionRegister
+    {
+        public static IServiceCollection AddExternalDevices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
